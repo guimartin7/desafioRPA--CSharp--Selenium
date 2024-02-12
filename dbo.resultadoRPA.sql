37e98b6765f31bad3a5b53cf9848702ba783462f
guimartin7 <99848607+guimartin7@users.noreply.github.com>
2024-02-11 23:42:03 -0300

    Guia de Funcionalidade
    Nesse commit foi adicionado os arquivos citados em e-mail para melhor validação. Dúvidas, me coloco à disposição.

---
+++ dbo.resultadoRPA.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[resultadoRPA] (
+    [COD]          INT          IDENTITY (1, 1) NOT NULL,
+    [wpm]          NUMERIC NULL,
+    [keystrokes]   NUMERIC NULL,
+    [accuracy]     NVARCHAR(50) NULL,
+    [correctWords] NUMERIC NULL,
+    [wrongWords]   NUMERIC NULL
+);
+
